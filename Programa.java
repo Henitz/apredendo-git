9a3579e9724f90f2bf69f193a80f0e3836b1e9bd
Henitz <33755649+Henitz@users.noreply.github.com>
2021-06-10 18:41:57 -0300

    Adicionando obrigado

--- Programa.java
+++ Programa.java
@@ -2,7 +2,7 @@ public class Programa{
 
 	public static void main(String[] args){
 		System.out.println("Ola " + args[0]);
-		System.out.println("Volte sempre");
+		System.out.println("Volte sempre - Obrigado");
 	}
 
 }
